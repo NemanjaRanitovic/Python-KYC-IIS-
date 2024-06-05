00139777e0fed7b2ee49a17dda7e569f0bfdc35b
NemanjaRanitovic <nemanjaranit@gmail.com>
2024-06-05 18:05:26 +0200

    A

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 import os
+import requests
+import pdfplumber
+import pytesseract
 
 app = FastAPI()
 
@@ -23,3 +26,20 @@ async def get_paths():
         for path in all_file_paths:
                 paths.append(path)
         return {"Paths:" : paths}
+
+@app.get("/readDox")
+async def read_dox():
+        lines = []
+        folder_path = r'C:\Users\Nemanja\Desktop\StudentRequests'
+        all_file_paths = get_all_file_paths(folder_path)
+        for path in all_file_paths:       
+                invoice_pdf = path
+                last_three_chars = invoice_pdf[-3:]
+                if last_three_chars == "pdf":
+                        os.system(f'ocrmypdf --force-ocr {invoice_pdf} output.pdf')
+                        with pdfplumber.open("output.pdf") as pdf:
+                                page = pdf.pages[0]
+                                text = page.extract_text()
+                        lines += text.split('\n')
+        return lines
+                
