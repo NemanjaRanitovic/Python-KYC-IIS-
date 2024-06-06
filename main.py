1a9f6161e6ea886382d67ec331132250c2da9d8c
NemanjaRanitovic <nemanjaranit@gmail.com>
2024-06-06 23:08:46 +0200

    B

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import json
 from fastapi import FastAPI
 import os
 import requests
 import pdfplumber
 import pytesseract
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+
+class Users(BaseModel):
+    Usernames: List[str]
 
 def get_all_file_paths(folder_path):
     file_paths = []
@@ -19,27 +25,33 @@ async def get_message():
         return {"Message" : "Hello World!"}
 
 @app.get("/getPaths")
-async def get_paths():
+async def get_paths(users : Users):
         paths = []
-        folder_path = r'C:\Users\Nemanja\Desktop\StudentRequests'
-        all_file_paths = get_all_file_paths(folder_path)
-        for path in all_file_paths:
-                paths.append(path)
+        for user in users.Usernames:
+                print(user)
+                folder_path = rf'C:\Users\Nemanja\Desktop\StudentRequests\{user}'
+                print(folder_path)
+                all_file_paths = get_all_file_paths(folder_path)
+                for path in all_file_paths:
+                        paths.append(path)
+
         return {"Paths:" : paths}
 
 @app.get("/readDox")
-async def read_dox():
-        lines = []
-        folder_path = r'C:\Users\Nemanja\Desktop\StudentRequests'
-        all_file_paths = get_all_file_paths(folder_path)
-        for path in all_file_paths:       
-                invoice_pdf = path
-                last_three_chars = invoice_pdf[-3:]
-                if last_three_chars == "pdf":
-                        os.system(f'ocrmypdf --force-ocr {invoice_pdf} output.pdf')
-                        with pdfplumber.open("output.pdf") as pdf:
-                                page = pdf.pages[0]
-                                text = page.extract_text()
-                        lines += text.split('\n')
+async def read_dox(users : Users):
+        for user in users.Usernames:
+                lines = []
+                folder_path = rf'C:\Users\Nemanja\Desktop\StudentRequests\{user}'
+                all_file_paths = get_all_file_paths(folder_path)
+                for path in all_file_paths:       
+                        invoice_pdf = path
+                        last_three_chars = invoice_pdf[-3:]
+                        if last_three_chars == "pdf":
+                                os.system(f'ocrmypdf --force-ocr {invoice_pdf} output.pdf')
+                                with pdfplumber.open("output.pdf") as pdf:
+                                        page = pdf.pages[0]
+                                        text = page.extract_text()
+                                lines += text.split('\n')
+                                json_string = json.dumps(lines)
         return lines
                 
